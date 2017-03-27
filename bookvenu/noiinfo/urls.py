a6a7a2f797828c7e03923dd1d841bd524d550663
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-03-27 21:48:30 +0300

    fix media url & root

--- bookvenu/noiinfo/urls.py
+++ bookvenu/noiinfo/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -22,4 +24,5 @@ urlpatterns = [
     url(r'^', include('authentication.urls')),
     url(r'^profile/', include('user_profile.urls')),
     url(r'^', include('post.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
