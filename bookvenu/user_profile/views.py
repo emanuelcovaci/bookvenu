cccd158068add6649ec918a9cc2b208c2e55d768
Mihai <mihai.temian@gmail.com>
2017-04-26 21:45:57 +0300

    edit profile

--- bookvenu/user_profile/views.py
+++ bookvenu/user_profile/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render,render_to_response
-from django.http import Http404
-from django.contrib.auth.models import User
-from forms import UserRegisterForm
+from django.shortcuts import render, redirect
+from forms import Edit_profile, Edit_profile2
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def profile_detail(request):
-        form = UserRegisterForm(data=request.POST or None)
-        if request.method == 'POST':
-            if form.is_valid() == True and acc_form.is_valid() == True:
-                form.instance.set_password(form.cleaned_data['password'])
-                form.save()
-        return render(request, "profile/profile-details.html", {
-            'form': form,
-        })
+    current_user = request.user
+    user_form = Edit_profile(data=request.POST or None,instance=current_user,user=current_user)
+    account_form = Edit_profile2(data=request.POST or None,instance=current_user.account)
+    if request.method == 'POST':
+        if user_form.is_valid() and account_form.is_valid():
+            user_form.save()
+            account_form.save()
+            return redirect('/')
+    return render(request, 'profile/profile-details.html', {
+        'form': user_form,
+        'account_form': account_form
+    })
+
+@login_required
 def history(request):
     return render(request, 'profile/profile-deals.html')
-def get_user_profile(request, username):
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        raise Http404("User does not exist")
-    return render(request, 'profile/profile-details.html', {"user":user})
